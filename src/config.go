4aa11e0ea49a3e9d606c395a4a896a121b08b737
Tong Sun <suntong@cpan.org>
2015-08-06 23:18:30 -0400

    - [*] use multiconfig to do the configuration instead

--- src/config.go
+++ src/config.go
@@ -4,44 +4,28 @@
 // Authors: Tong Sun (c) 2015, All rights reserved
 ////////////////////////////////////////////////////////////////////////////
 
-// Style: gofmt -tabs=false -tabwidth=2 -w
-
 /*
-
-Based on https://github.com/pkieltyka/godo-app/blob/master/config.go
-by Peter Kieltyka. Enhanced so that,
 
 Application configuration that satisfies:
 
-- Have a set of default values defined in the program
-- Variables defined in the config file will override them
+- Have a set of default values defined in the config file
+- Environment variables can override them
 - Variables passed from the command line takes the highest priority
-
-Next time:
- Maybe http://spf13.com/project/viper from hugo is a better option.
 
 */
 
 package main
 
 import (
-	"errors"
 	"flag"
-	"fmt"
-	"log"
-	"os"
 	"runtime"
 
-	"github.com/BurntSushi/toml"
+	"github.com/koding/multiconfig"
 )
 
 var (
-	configFile = flag.String("Conf", "simplicity.conf", "path to config file")
-
-	maxProcs = flag.Int("MaxProcs", -1, "GOMAXPROCS, default is NumCpu()")
+	configFile = flag.String("conf", "simplicity.toml", "path to config file")
 )
-
-var ErrNoConfigFile = errors.New("No configuration file specified.")
 
 type Config struct {
 	MaxProcs int
@@ -54,73 +38,21 @@ type Config struct {
 	}
 }
 
-func usage() {
-	// Fprintf allows us to print to a specifed file handle or stream
-	fmt.Fprintf(os.Stderr, "\nUsage: %s [flags ...]\n\n",
-		os.Args[0])
-	flag.PrintDefaults()
-	os.Exit(0)
-}
-
+// ConfigGet will get configuration from multiple layers, toml file, environment variables, or command line.
 func ConfigGet() *Config {
-	var err error
-	var cf *Config = NewConfig()
-
-	// set default values defined in the program
-	cf.ConfigFromFlag()
-	//log.Printf("P: %d, B: '%s', F: '%s'\n", cf.MaxProcs, cf.Webapp.Path)
-
-	// Load config file, from flag or env (if specified)
-	_, err = cf.ConfigFromFile(*configFile, os.Getenv("APPCONFIG"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	//log.Printf("P: %d, B: '%s', F: '%s'\n", cf.MaxProcs, cf.Webapp.Path)
-
-	// Override values from command line flags
-	cf.ConfigToFlag()
-	flag.Usage = usage
-	flag.Parse()
-	cf.ConfigFromFlag()
-	//log.Printf("P: %d, B: '%s', F: '%s'\n", cf.MaxProcs, cf.Webapp.Path)
+	// Start with an empty struct of the configuration
+	cf := new(Config)
+	m := multiconfig.NewWithPath(*configFile)
+	// Populated the configuration struct
+	m.MustLoad(cf) // Check for error
+	//log.Printf("%+v\n", cf)
+	//log.Printf("P: %d, B: '%s', F: '%s'\n", cf.MaxProcs, cf.Webapp.Path, *configFile)
 
 	cf.ConfigApply()
-
 	return cf
 }
 
-func NewConfig() *Config {
-	return &Config{}
-}
-
-func NewConfigFromFile(confFile string, confEnv string) (*Config, error) {
-	cf := &Config{}
-	return cf.ConfigFromFile(confFile, confEnv)
-}
-
-func (cf *Config) ConfigFromFile(confFile string, confEnv string) (*Config, error) {
-	if confFile == "" {
-		confFile = confEnv
-	}
-
-	if _, err := os.Stat(confFile); os.IsNotExist(err) {
-		return nil, ErrNoConfigFile
-	}
-
-	if _, err := toml.DecodeFile(confFile, &cf); err != nil {
-		return nil, err
-	}
-	return cf, nil
-}
-
-func (cf *Config) ConfigFromFlag() {
-	cf.MaxProcs = *maxProcs
-}
-
-func (cf *Config) ConfigToFlag() {
-	*maxProcs = cf.MaxProcs
-}
-
+// ConfigApply will apply the configuration
 func (cf *Config) ConfigApply() {
 	if cf.MaxProcs <= 0 {
 		cf.MaxProcs = runtime.NumCPU()
